cc9177e3aad3a64f2b09b16949f9937f0c686b8a
Yury Luneff <yury@indigosystem.ru>
2010-02-27 22:36:09 +0300

    changes после того, как восстановили

--- src/main/java/t5/DiskApplet.java
+++ src/main/java/t5/DiskApplet.java
@@ -8,7 +8,9 @@ package t5;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.io.*;
+import java.util.*;
+import java.net.URL;
 import javax.swing.*;
 
 public class DiskApplet extends JApplet {
@@ -33,7 +35,7 @@ public class DiskApplet extends JApplet {
 
     public void setupAndRunGui() {
         setSize(89, 33);
-        openButton = new JButton("Найти файлы");
+        openButton = new JButton("Open dir");
         openButton.setSize(89, 33);
         openButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
@@ -62,5 +64,94 @@ public class DiskApplet extends JApplet {
         task.setMaster(this);
         task.setFile(file);
         task.start();
+    }
+
+    private class Task extends Thread {
+
+        private File file;
+        private DiskApplet master;
+
+        Task() {
+        }
+
+        public void run() {
+            try {
+                List<File> files = getFileListing(file);
+                File temp = File.createTempFile("diskApplet", null);
+                PrintWriter pw = new PrintWriter(new FileOutputStream(temp));
+                long len = file.getAbsolutePath().length();
+                for (File file : files) {
+                    pw.println(file.getAbsolutePath());
+                    pw.println(file.length());
+                }
+                pw.flush();
+                pw.close();
+                InputStream is = ClientHttpRequest.post(new URL(master.getParameter("uploadUrl")), "files", temp);
+                is.close();
+                temp.deleteOnExit();
+                try {
+                    master.getAppletContext().showDocument(new URL(master.getParameter("redirectUrl")), "_top");
+                } catch (Exception e) {
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Попытка деления на букву О");
+                e.printStackTrace();
+            }
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        public void setFile(File file) {
+            this.file = file;
+        }
+
+        public List getFileListing(File aStartingDir)
+                throws FileNotFoundException {
+            validateDirectory(aStartingDir);
+            List result = getFileListingNoSort(aStartingDir);
+            Collections.sort(result);
+            return result;
+        }
+
+        private List getFileListingNoSort(File aStartingDir)
+                throws FileNotFoundException {
+            List result = new ArrayList();
+            File filesAndDirs[] = aStartingDir.listFiles();
+            List filesDirs = Arrays.asList(filesAndDirs);
+            Iterator i$ = filesDirs.iterator();
+            do {
+                if (!i$.hasNext())
+                    break;
+                File file = (File) i$.next();
+                result.add(file);
+                if (!file.isFile()) {
+                    List deeperList = getFileListingNoSort(file);
+                    result.addAll(deeperList);
+                }
+            } while (true);
+            return result;
+        }
+
+        private void validateDirectory(File aDirectory)
+                throws FileNotFoundException {
+            if (aDirectory == null)
+                throw new IllegalArgumentException("Directory should not be null.");
+            if (!aDirectory.exists())
+                throw new FileNotFoundException((new StringBuilder()).append("Directory does not exist: ").append(aDirectory).toString());
+            if (!aDirectory.isDirectory())
+                throw new IllegalArgumentException((new StringBuilder()).append("Is not a directory: ").append(aDirectory).toString());
+            if (!aDirectory.canRead())
+                throw new IllegalArgumentException((new StringBuilder()).append("Directory cannot be read: ").append(aDirectory).toString());
+        }
+
+        public DiskApplet getMaster() {
+            return master;
+        }
+
+        public void setMaster(DiskApplet master) {
+            this.master = master;
+        }
     }
 }
